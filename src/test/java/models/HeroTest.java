a985f78c3f5754f704468a0c69187f55ec86eb8f
Francisndungu2 <francianmbugua@gmail.com>
2020-11-29 20:41:21 +0300

    addtest

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,4 +1,26 @@
 package models;
 
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 public class HeroTest {
+    @After
+    public void clearInstances(){
+        Hero.clearAllHeroes();
+    }
+
+    @Test
+    public void Hero_InstantiatesCorrectly_true() throws Exception {
+        Hero myHero = new Hero("Me", 25, "Wits", "LWK", 1);
+        assertTrue(myHero instanceof Hero);
+    }
+
+    public Hero setUpNewHero() {
+
+        return new Hero("Batman", 32, "Sulking", "Robin", 1);
+    }
 }
