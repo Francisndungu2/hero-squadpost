6cc041f1e95affeb5837d245b12cb6ae5d919403
Francisndungu2 <francianmbugua@gmail.com>
2020-11-29 20:24:24 +0300

    addimports

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,13 @@
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
 }
