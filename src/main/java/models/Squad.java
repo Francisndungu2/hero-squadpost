81389ae2180bd6ccff98b3967a1282270b35b214
Francisndungu2 <francianmbugua@gmail.com>
2020-11-29 20:21:03 +0300

    add clearall

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -12,5 +12,58 @@ public class Squad {
     private int id;
 
 
+    public Squad(String squadName, String cause, int maxSize) {
+        this.squadName = squadName;
+        this.cause = cause;
+        this.maxSize = maxSize;
+        instances.add(this);
+        this.id = instances.size();
+}
+}
+
+    public void addMember(Hero hero){
+        squadMembers.add(hero);
+    }
+
+    public List<Hero> getSquadMembers() {
+        return squadMembers;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+    public String getCause() {
+        return cause;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public static ArrayList<Squad> getAll() {
+        return instances;
+    }
+
+    public static void clearAllSquads(){
+        instances.clear();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public static Squad findById(int id) {
+        return instances.get(id-1);
+    }
+
+    public void update(String squadName, String cause , int maxSize) {
+        this.squadName = squadName;
+        this.cause = cause;
+        this.maxSize = maxSize;
+    }
+
+    public void deleteSquad(){
+        instances.remove(id-1);
+    }
+
 
 }
